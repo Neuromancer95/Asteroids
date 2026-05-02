da8fec5efcf6f9f9318cb6e5db212d42c825e89d
Neuromancer95 <bpricks@live.com>
2026-05-02 12:21:06 -0700

    A: first add for all files

---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame
+from constants import SCREEN_WIDTH
+from constants import SCREEN_HEIGHT
+from logger import log_state
+from player import Player
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group() 
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
+    while True: 
+        dt = clock.tick(60) / 1000
+        log_state()
+        for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+               return 
+        screen.fill("black")
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+        pygame.display.flip()
+              
+
+if __name__ == "__main__":
+    main()
